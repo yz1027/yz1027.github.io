d916bcaec4b0a44c92b155df1c7b54736800735e
赵岩 <sezhaoyan@mail.scut.edu.cn>
2026-02-20 19:19:50 +0800

    时间魔法5

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -37,7 +37,7 @@ export default defineConfig({
           { text: '三', link: '/novels/timemagic/timemagic3' },
           { text: '三又五 死灵魔法', link:'/novels/timemagic/timemagic3-5-x'},
           { text: '四 命运魔法', link: '/novels/timemagic/timemagic4' },
-          { text: '四 命运魔法', link: '/novels/timemagic/timemagic5' },
+          { text: '五 时间魔法', link: '/novels/timemagic/timemagic5' },
         ]
       },
       { 
